12e26c21455bbe66b9e2192d2540450b519aa604
A1iHassan <ali012wkout@gmail.com>
2026-08-06 18:17:34 +0300

    styling the news section

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { ChevronDownIcon, GlobeAltIcon, AcademicCapIcon, UserGroupIcon, BookOpenIcon, BeakerIcon, PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, ChevronLeftIcon, GlobeAltIcon, AcademicCapIcon, UserGroupIcon, BookOpenIcon, BeakerIcon, PhoneIcon, EnvelopeIcon, MapPinIcon, ArrowLongLeftIcon } from '@heroicons/react/24/outline'
 
 const HomePage = () => {
   const [scrolled, setScrolled] = useState(false)
@@ -61,45 +61,84 @@ const HomePage = () => {
 
 
       {/* Latest News Section */}
-      <section className="mx-auto grid h-screen w-full gap-8 px-6 py-16 md:grid-cols-5">
-        <article className="relative col-span-3 overflow-hidden shadow-xl/40 bg-white">
+      <section className="mx-auto grid h-screen w-full gap-8 px-6 py-32 md:grid-cols-5">
+        <article className="group relative col-span-3 overflow-hidden shadow-xl/40 bg-white mr-5 cursor-pointer">
           {/* Latest published news report */}
           <div className="overflow-hidden h-full relative z-10">
-            <img src="/Latest.jpeg" alt="Latest News" className="object-cover w-full h-full transition-transform duration-500" />
+            <img src="/Latest.jpeg" alt="Latest News" className="object-cover w-full h-full group-hover:scale-105 transition-transform duration-500" />
             {/* Dark overlay on the video */}
-            <div className="absolute inset-0 bg-gradient-to-t from-slate-950/80 via-slate-900/40 to-slate-900/30" />
+            <div className="absolute inset-0 bg-gradient-to-t from-slate-950/80 via-slate-900/40 to-slate-900/30 transition-opacity duration-500 group-hover:bg-slate-900/50 transition-color duration-200" />
           </div>
-          <div className="p-6 z-100 absolute bottom-0">
-            <h2 className="mb-3 text-5xl font-bold text-slate-100">Latest News Report</h2>
-            <p className="text-xl leading-relaxed text-slate-200">News summary content...</p>
+          <div className="p-6 z-100 absolute bottom-0 w-full flex justify-between items-end">
+            <div className="text-right">
+              <h2 className="mb-3 text-5xl font-bold text-slate-100">Latest News Report</h2>
+              <p className="text-xl leading-relaxed text-slate-200">...News summary content</p>
+            </div>
+            <div className="overflow-hidden mb-2">
+              <div className="flex items-center gap-2 text-slate-100 font-semibold text-lg translate-x-[100%] transition-transform duration-500 group-hover:translate-x-0">
+                <ArrowLongLeftIcon className="h-6 w-6" />
+                <span>Read More</span>
+              </div>
+            </div>
           </div>
         </article>
         
-        <aside className="col-span-2 flex flex-col gap-4">
-          <article className="relative group flex-1 overflow-hidden rounded-2xl border border-slate-200 bg-white p-5 shadow-sm transition-shadow duration-300 hover:shadow-lg">
-          <div className="overflow-hidden relative z-10">
-            <img src="/Latest.jpeg" alt="Latest News" className="object-cover w-full h-full transition-transform duration-500" />
-            {/* Dark overlay on the video */}
-            <div className="absolute inset-0 bg-gradient-to-t from-slate-950/80 via-slate-900/40 to-slate-900/30" />
-          </div>
-          <div className="p-6 z-100 absolute bottom-0">
-            <h2 className="mb-3 text-5xl font-bold text-slate-100">Latest News Report</h2>
-            <p className="text-xl leading-relaxed text-slate-200">News summary content...</p>
-          </div>
+        <aside className="col-span-2 flex flex-col items-center gap-4">
+          <article className="group relative flex-1 overflow-hidden bg-white p-5 shadow-xl/40 cursor-pointer max-h-1/3 w-5/6">
+	    {/* Background Image & Overlay (Removed from document flow) */}
+	    <img
+		src="/Latest.jpeg" 
+		alt="Article Background"
+		className="absolute inset-0 h-full w-full object-cover z-0 transition-transform duration-500 group-hover:scale-105" />
+  	    <div className="absolute inset-0 bg-slate-900/70 z-0 transition-opacity duration-500 group-hover:bg-slate-900/80" />
+	    <div className='relative z-10 flex h-full flex-col justify-end'>
+           <div className="w-full flex justify-between items-end">
+             <div className="overflow-hidden mb-1">
+               <div className="flex items-center gap-2 text-slate-100 font-semibold translate-x-[100%] transition-transform duration-500 group-hover:translate-x-0">
+                 <ArrowLongLeftIcon className="h-5 w-5" />
+                 <span>Read More</span>
+               </div>
+             </div>
+             <div className="text-right">
+               <h2 className="mb-2 text-3xl font-bold text-slate-100">Latest News Report</h2>
+               <p className="text-lg leading-relaxed text-slate-200">...News summary content</p>
+             </div>
+           </div>
+	    </div>
           </article>
-          <article className="relative group flex-1 overflow-hidden rounded-2xl border border-slate-200 bg-white p-5 shadow-sm transition-shadow duration-300 hover:shadow-lg">
-          <div className="overflow-hidden relative z-10">
-            <img src="/Latest.jpeg" alt="Latest News" className="object-cover w-full h-full transition-transform duration-500" />
-            {/* Dark overlay on the video */}
-            <div className="absolute inset-0 bg-gradient-to-t from-slate-950/80 via-slate-900/40 to-slate-900/30" />
-          </div>
-          <div className="p-6 z-100 absolute bottom-0">
-            <h2 className="mb-3 text-5xl font-bold text-slate-100">Latest News Report</h2>
-            <p className="text-xl leading-relaxed text-slate-200">News summary content...</p>
-          </div>
+          <article className="group relative flex-1 overflow-hidden bg-white p-5 shadow-xl/40 cursor-pointer max-h-1/3 w-5/6">
+	    {/* Background Image & Overlay (Removed from document flow) */}
+	    <img
+		src="/Latest.jpeg" 
+		alt="Article Background"
+		className="absolute inset-0 h-full w-full object-cover z-0 transition-transform duration-500 group-hover:scale-105" />
+  	    <div className="absolute inset-0 bg-slate-900/70 z-0 transition-opacity duration-500 group-hover:bg-slate-900/80"></div>
+	    <div className='relative z-10 flex h-full flex-col justify-end'>
+           <div className="w-full flex justify-between items-end">
+             <div className="overflow-hidden mb-1">
+               <div className="flex items-center gap-2 text-slate-100 font-semibold translate-x-[100%] transition-transform duration-500 group-hover:translate-x-0">
+                 <ArrowLongLeftIcon className="h-5 w-5" />
+                 <span>Read More</span>
+               </div>
+             </div>
+             <div className="text-right">
+               <h2 className="mb-2 text-3xl font-bold text-slate-100">Latest News Report</h2>
+               <p className="text-lg leading-relaxed text-slate-200">...News summary content</p>
+             </div>
+           </div>
+	    </div>
           </article>
           
-          <button className="mt-auto w-full rounded-xl border-2 border-slate-300 bg-slate-50 px-6 py-3 text-sm font-semibold text-slate-700 transition-all duration-300 hover:border-slate-500 hover:bg-slate-800 hover:text-white cursor-pointer">See more of our news</button>
+          <button className="group relative self-start mr-35 mt-12 flex items-center text-lg font-semibold text-slate-700 cursor-pointer pb-1">
+            <div className="flex items-center transition-all duration-300 w-0 overflow-hidden group-hover:w-6 opacity-0 group-hover:opacity-100 group-hover:mr-1">
+              <ChevronLeftIcon className="h-5 w-5 text-slate-700 flex-shrink-0" />
+            </div>
+            <span>See more of our news</span>
+            <div className="absolute bottom-0 right-0 w-full flex flex-col gap-[2px] mt-5">
+              <div className="h-[2px] w-full bg-slate-700 transition-all duration-300"></div>
+              <div className="h-[2px] w-2/3 bg-slate-700 transition-all duration-300"></div>
+            </div>
+          </button>
         </aside>
       </section>
 
